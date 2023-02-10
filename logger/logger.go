4af773c7c5f4e9dc7403a0ff0fe2c0e6e4c526f2
Kaustubh Murumkar <kaustubh.murumkar1@gmail.com>
2023-02-11 03:09:50 +0530

    update logger to use default logger

--- logger/logger.go
+++ logger/logger.go
@@ -12,10 +12,10 @@ var (
 	osOpenFile = os.OpenFile
 )
 
-func Init(filePath string, isProduction bool) (*logrus.Logger, error) {
+func Init(filePath string, isProduction bool) error {
 	logFile, err := osOpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, fs.FileMode(644))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	logWriter := io.MultiWriter(logFile, os.Stdout)
@@ -26,11 +26,9 @@ func Init(filePath string, isProduction bool) (*logrus.Logger, error) {
 		formatter = &logrus.TextFormatter{}
 		level = logrus.ErrorLevel
 	}
-	logger := &logrus.Logger{
-		Out:       logWriter,
-		Formatter: formatter,
-		Level:     level,
-	}
+	logrus.SetFormatter(formatter)
+	logrus.SetOutput(logWriter)
+	logrus.SetLevel(level)
 
-	return logger, nil
+	return nil
 }
